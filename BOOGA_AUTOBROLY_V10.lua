037ea1bdbcf12882f927106364d1c2840afa475a
imkizz07 <168879215+imkizz07@users.noreply.github.com>
2024-05-09 08:19:49 +0100

    Update BOOGA_AUTOBROLY_V10.lua

--- BOOGA_AUTOBROLY_V10.lua
+++ BOOGA_AUTOBROLY_V10.lua
@@ -17,7 +17,7 @@ Settings = {
     PadChangerLimit = 5; -- [[ if someone has been in your pad 5 times, you will stop changing pads ]]
     FpsBoost = false; -- [[ Low your graphics to get a fps boost ]]
     Anchored = false; -- [[ Will freeze you on queue and broly world,enable broly anchored reccommended if you enable this ]]
-    BrolyAnchored = true; -- [[ Will freeze broly ]]
+    BrolyAnchored = false; -- [[ Will freeze broly ]]
 }
 
 TimeSettings = {
@@ -30,11 +30,11 @@ Moves = {
     "Meteor Crash";
     "Anger Rush";
     "Deadly Dance";
-    "";
+    "Flash Strike";
     "Neo Wolf Fang Fist";
     "God Slicer";
-    "";
-    "";
+    "Mach Kick";
+    "Punisher Drive";
     "";
     "";
     "";
