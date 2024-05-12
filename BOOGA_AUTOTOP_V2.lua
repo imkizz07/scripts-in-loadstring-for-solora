d1168691c5f79477a030e7176818b7bda380c253
imkizz07 <168879215+imkizz07@users.noreply.github.com>
2024-05-12 13:26:31 +0100

    Update BOOGA_AUTOTOP_V2.lua

--- BOOGA_AUTOTOP_V2.lua
+++ BOOGA_AUTOTOP_V2.lua
@@ -13,10 +13,10 @@ Moves = { -- You can put all the moves that you want
 	"Strong Kick"
 }
 Settings = {
-    AutoWish = false;
-    Wish = "Nothing"; -- Zenni = 100k Zenni,Prestige = Prestige,EXP = More exp,Level Cap = Unlock Level Cap,God = Unlock God Form,Death = Go To Heaven,Nothing = nothing
+    AutoWish = true;
+    Wish = "Zeni"; -- Zenni = 100k Zenni,Prestige = Prestige,EXP = More exp,Level Cap = Unlock Level Cap,God = Unlock God Form,Death = Go To Heaven,Nothing = nothing
     RejoinTime = 1200;-- If TOP is not completed in this time you will be teleported to queue
-    TOP = "Hard"; -- Normal = Normal TOP,Hard = Hard TOP
+    TOP = "Normal"; -- Normal = Normal TOP,Hard = Hard TOP
     ChargeTime = 4;-- Charge Time In Seconds
     FormType = "h"; -- Form Key
     SpamBeans = true; -- true = will spam beans,false = won't spam beans
